5f6bb05eee02330753929c274fd70601ca52f92a
Zoechbauer <hans.zoechbauer@aon.at>
2020-03-18 12:36:09 +0100

    Retset Form to default values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -49,5 +49,10 @@ export class AppComponent {
     this.user.secretQuestion = this.signupForm.value.secret;
     this.user.answer = this.signupForm.value.questionAnswer;
     this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset({
+      gender: 'male',
+      secret: 'pet'
+    });
   }
 }
